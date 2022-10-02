a9c4fd8d1e0d8c119071795275e4af8885b6854e
aaryansr26 <aaryansr26@gmail.com>
2022-10-02 12:01:45 +0530

    added cryptography module for encryption algorithms

--- src/utils/add.py
+++ src/utils/add.py
@@ -1,13 +1,16 @@
 
 from getpass import getpass
+from Crypto.Protocol.KDF import PBKDF2
 
 def computeMasterKey(mp, ds):
     password = mp.encode()
     salt = ds.encode()
+    key =
 
 
 def addEntry(mp, ds, sitename, siteurl, email, username):
     #get the password
     password = getpass("Password: ")
     mk = computeMasterKey(mp, ds)
+    
     
