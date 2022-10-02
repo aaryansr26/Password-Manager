81e310af1b7fdab540330d6b9109c21a4c5647bb
aaryansr26 <aaryansr26@gmail.com>
2022-10-03 01:38:11 +0530

    test and config changes

--- src/test.py
+++ src/test.py
@@ -8,24 +8,14 @@ from utils.aesutil import encrypt,decrypt
 from rich.console import Console
 from rich import print as printc
 
+from utils import dbconfig
+
 console = Console()
 
 
-if __name__=="__main__":
-    
-    op = sys.argv[1]
-    
-    if op==1 or op=="encrypt":
-        msg = sys.argv[2]
-        key = sys.argv[3]
-        keyType = sys.argv[4]
-        cipher, newkey = encrypt(key, msg, keyType=keyType)
-        print(cipher, newkey)
-        
-    
-    elif op==2 or op == "decrypt":
-        cipher = sys.argv[2]
-        key = sys.argv[3]
-        keyType = sys.argv[4]
-        plaintext = decrypt(key, cipher, keyType=keyType)
-        print(plaintext)
+db = dbconfig.dbconfig()
+cursor = db.cursor()
+query = "select * from pm.entries"
+cursor.execute(query)
+
+print(cursor.fetchall()[0][4])
