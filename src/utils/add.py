a46a26c91f72ea2316cb22d558c6b4d5fcb5211e
aaryansr26 <aaryansr26@gmail.com>
2022-10-02 12:12:01 +0530

    added functionality for computing Master Key

--- src/utils/add.py
+++ src/utils/add.py
@@ -1,16 +1,20 @@
 
 from getpass import getpass
 from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA512
+from Crypto.Random import get_random_bytes
 
 def computeMasterKey(mp, ds):
     password = mp.encode()
     salt = ds.encode()
-    key =
+    key = PBKDF2(password, salt, 32, count=10000000, hmac_hash_module=SHA512)
+    return key
 
 
 def addEntry(mp, ds, sitename, siteurl, email, username):
     #get the password
     password = getpass("Password: ")
     mk = computeMasterKey(mp, ds)
+    
     
     
