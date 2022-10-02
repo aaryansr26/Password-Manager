3e48e43a3dfa602508d058235469490467000183
aaryansr26 <aaryansr26@gmail.com>
2022-10-02 20:03:05 +0530

    finalized add.py function

--- src/utils/add.py
+++ src/utils/add.py
@@ -1,8 +1,11 @@
-
+import utils.aesutil
+from utils.dbconfig import dbconfig
 from getpass import getpass
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA512
 from Crypto.Random import get_random_bytes
+import utils.aesutil
+from rich import print as printc
 
 def computeMasterKey(mp, ds):
     password = mp.encode()
@@ -16,5 +19,17 @@ def addEntry(mp, ds, sitename, siteurl, email, username):
     password = getpass("Password: ")
     mk = computeMasterKey(mp, ds)
     
+    encrypted_key = utils.aesutil.encrypt(key=mk, source=password, keyType="bytes")
     
+    #add the password to Database
+    db = dbconfig()
+    cursor = db.cursor()
     
+    query = "INSERT INTO pm.entries (sitename, siteurl, email, username, password) values (%s, %s, %s, %s, %s)"
+    val = (sitename, siteurl, email, username, password)
+    
+    cursor.execute(query, val)
+    db.commit()
+    printc("[green][+][/green] Entry added in db ")
+    
+        
